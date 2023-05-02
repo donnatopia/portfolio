8ad7dfd5c4c5b1d8c1dea55ee18f38c429f95965
Donna <donnatopia@gmail.com>
2023-05-02 11:53:08 -0700

    create components index folder

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as About } from './About.jsx';
+export { default as Main } from './Main.jsx';
+export { default as Navbar } from './Navbar.jsx';
+export { default as Skills } from './Skills.jsx';
