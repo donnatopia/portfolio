85cdc48cb20f4262c95a531ce64fc35e5a598427
Donna <donnatopia@gmail.com>
2023-05-01 19:26:05 -0700

    fixed apostrophe

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className='pt-40'>
-              <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+              <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s Connect</p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:1scale-105 ease-in duration-300'>
                   <FaLinkedinIn />
