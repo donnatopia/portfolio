04b60ef8e81b68957d33703ffd7aaf5a11ee5bc6
Donna <donnatopia@gmail.com>
2023-05-03 18:22:10 -0700

    added autoplay

--- components/Skills/index.jsx
+++ components/Skills/index.jsx
@@ -17,17 +17,17 @@ export default function Skills() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true
         }
       },
       {
@@ -35,14 +35,13 @@ export default function Skills() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       }
     ]
