04de546cce16b2ec78479f77ad94263e1cbdc065
Donna <donnatopia@gmail.com>
2023-05-04 15:17:39 -0700

    add links to contact section

--- components/Contact/Socials.jsx
+++ components/Contact/Socials.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiOutlineMail } from 'react-icons/ai';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
@@ -9,16 +10,19 @@ export default function Socials() {
       <p className='uppercase text-gray-500 text-sm text-center'>Connect with Me</p>
       <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
         <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-          <FaLinkedinIn />
+          <Link href='https://www.linkedin.com/in/donnawong515/' target='_blank'>
+            <FaLinkedinIn />
+          </Link>
         </div>
         <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-          <FaGithub />
+          <Link href='https://github.com/donnatopia' target='_blank'>
+            <FaGithub />
+          </Link>
         </div>
         <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-          <AiOutlineMail />
-        </div>
-        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-          <BsFillPersonLinesFill />
+          <Link href='mailto:donnaw515.dw@gmail.com' target='_blank'>
+            <AiOutlineMail />
+          </Link>
         </div>
       </div>
     </div>
