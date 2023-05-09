ecae008e61faf4d3751e3b7a6bbba46a4d65ac28
Donna <donnatopia@gmail.com>
2023-05-09 13:51:51 -0700

    fix bug with darkmode button

--- components/Layouts/DarkButton.jsx
+++ components/Layouts/DarkButton.jsx
@@ -2,23 +2,18 @@ import React, { useState } from 'react';
 import Switch from 'react-switch';
 import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 import { useTheme } from 'next-themes';
-import { useMediaQuery } from 'react-responsive';
 
 export default function DarkButton() {
   const { theme, setTheme } = useTheme();
-  const [isDarkMode, setIsDarkMode] = useState(
-    useMediaQuery({query: '(prefers-color-scheme: light)'})
-  );
 
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    setTheme(theme === 'dark' ? 'light' : 'dark')
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   }
 
   return (
     <Switch
       onChange={ toggleDarkMode }
-      checked={ isDarkMode }
+      checked={ theme === 'dark' }
       uncheckedIcon={
         <BsFillSunFill className='m-auto h-7 text-light-main-bg' />
       }
